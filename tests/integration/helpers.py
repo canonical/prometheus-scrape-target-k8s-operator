d5d0c1db05fb0c5cc0214c2901a1d9f3a2c8ce53
sed-i <82407168+sed-i@users.noreply.github.com>
2022-01-10 16:19:39 -0500

    add docstring

--- tests/integration/helpers.py
+++ tests/integration/helpers.py
@@ -12,5 +12,6 @@ async def get_unit_address(ops_test, app_name: str, unit_num: int) -> str:
 
 
 async def get_config_values(ops_test, app_name) -> dict:
+    """Return the app's config, but filter out keys that do not have a value."""
     config = await ops_test.model.applications[app_name].get_config()
     return {key: config[key]["value"] for key in config if "value" in config[key]}
